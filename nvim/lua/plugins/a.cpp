1340b0417602b5b5ebe2385cb379967ed8dc0416
MontMarcos <marcosvi896@gmail.com>
2025-09-26 11:58:31 -0300

    Adiciona arquivos de configuração inicial do Neovim

---
+++ nvim/lua/plugins/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+
+int main() {
+  int n;
+  for (int i = 0; i < n; i++) {
+    cin >> n;
+    cout << i << '\n';
+  }
+}
---
+++ nvim/lua/plugins/example.lua
@@ -0,0 +1,169 @@
+return {
+  -- ===================== THEME =====================
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "dark",
+      })
+      require("onedark").load() -- aqui termina de carregar o tema
+
+      -- ===================== FUNDO MAIS ESCURO =====================
+      vim.api.nvim_set_hl(0, "Normal", { bg = "#121212" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#121212" })
+      vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#121212" })
+      -- ================================================================
+    end,
+  },
+
+  -- ===================== TERMINAL =====================
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+  },
+
+  -- ===================== SNIPPETS =====================
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets" })
+    end,
+  },
+
+  -- ===================== CMP =====================
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+      "L3MON4D3/LuaSnip",
+    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      -- adicionar fonte de emoji
+      table.insert(opts.sources, { name = "emoji" })
+
+      -- navegação com Tab / Shift-Tab
+      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
+      opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+    end,
+  },
+
+  -- ===================== TELESCOPE =====================
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({
+            cwd = require("lazy.core.config").options.root,
+          })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
+
+  -- ===================== LSP =====================
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {},
+        -- tsserver = {}, <-- REMOVEMOS TSSEVER E SETUP: o extra do LazyVim (abaixo) cuidará disso
+      },
+      -- REMOVEMOS O BLOCO setup = { ... } INTEIRO
+    },
+    dependencies = {
+      "jose-elias-alvarez/typescript.nvim",
+      init = function()
+        require("lazyvim.util").lsp.on_attach(function(_, buffer)
+          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { buffer = buffer, desc = "Rename File" })
+        end)
+      end,
+    },
+  },
+
+  -- Importar extras DEPOIS de configurar o lspconfig.
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  -- ===================== LSP Extras (imports) =====================
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+
+  -- ===================== TREESITTER =====================
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      })
+    end,
+  },
+
+  -- ===================== LUALINE =====================
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, {
+        function()
+          return "😄"
+        end,
+      })
+    end,
+  },
+
+  -- ===================== TOOLS =====================
+  {
+    "mason.nvim", -- Nome corrigido
+    opts = {
+      ensure_installed = { "stylua", "shellcheck", "shfmt", "flake8" },
+    },
+  },
+}
